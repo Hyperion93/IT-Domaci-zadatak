78aec90ae28a7d9eba3ce5d828c4cced17583deb
Slovak <vslovak993@gmail.com>
2019-10-28 23:03:50 +0100

    uradjen domaci N2 pon

---
+++ Nedelja2/Cetvrtak/zadatak2.js
@@ -0,0 +1,13 @@
+//Domaci zadatak 2!!!
+
+var varDomaci = 'Welcome to DOM';
+
+const menjamNaslov = document.getElementById('naslovTest');
+menjamNaslov.innerHTML = varDomaci;
+
+//==================================================================
+
+const parag = document.getElementById('ubacPar');
+parag.innerHTML = '<p>durdurdurdurdurdurdurdurdurdur<br>durdurdurdurdurdurdurdurdurdur<br>durdurdurdurdurdurdurdurdurdur</p>';
+
+
---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,10 @@
+//domaci zadatak 1
+//========================================================
+var a=6;
+var b=4;
+console.log(`zbir ${a+b}`);
+console.log(`razlika ${a-b}`);
+console.log(`proizvod ${a*b}`);
+console.log(`kolicnik ${a/b}`);
+console.log(`ostatak pri deljenju ${a%b}`);
+console.log(`stepenovanje ${Math.pow(a, b)}`); // umesto Math.pow(a, b), moze se koristiti samo ${a ** b}  !!!!!!
