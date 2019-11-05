d4bd7eae338a0d5ed389ea3a824b566a86df7958
Slovak <vslovak993@gmail.com>
2019-11-05 19:31:51 +0100

    uradjen domaci za sredu

--- Nedelja3/zadatak1.js
+++ Nedelja3/zadatak1.js
@@ -1,13 +1,13 @@
 function brmax(a, b, c) {
-    if (a > b, a > c){
-         console.log(`najveci broj je ${a}`);
+    if (a > b && a > c){
+    console.log(`najveci broj je ${a}`);
     }
-    if (b > a, b > c) {
+    if (b > a && b > c) {
     console.log(`najveci broj je ${b}`);
     }
-    if (c > a, c > b) {
+    if (c > a && c > b) {
     console.log(`najveci broj je ${c}`); 
     }
     return brmax;
 }
-brmax(4, 40,12);
+brmax(899,70,555);
