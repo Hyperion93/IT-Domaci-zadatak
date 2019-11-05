c3207fb673bc492823e1e8095eff36f367429650
Slovak <vslovak993@gmail.com>
2019-11-05 19:20:54 +0100

    uradjen domaci za sredu

---
+++ Nedelja3/zadatak2.js
@@ -0,0 +1,5 @@
+ function cenapice(cena, poluprecnik){
+     let povrs=(poluprecnik ** 2) * Math.PI;
+     return cenapice = povrs/cena;
+    }
+console.log('Cena pice po cm^2 je '+cenapice(500, 16));
